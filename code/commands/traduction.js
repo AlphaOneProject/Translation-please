cd2e34822228b67aa38ab24ff607928580b00001
Antoine Félix <felixantoine67@yahoo.fr>
2020-06-08 00:08:18 +0200

    UPDATED full functionality

--- code/commands/traduction.js
+++ code/commands/traduction.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 
 module.exports = {
-    name: "translation",
-    description: "Asks you to translate a french word in english.",
+    name: "traduction",
+    description: "Vous demande de traduire un mot anglais en français.",
     execute(message) {
         const dictionary = require('../dictionary.json');
         let selected_id = Math.floor(Math.random() * dictionary.words.length);
         let word = dictionary.words[selected_id];
 
-        message.channel.send("Translate " + word.french + " in english!");
+        message.channel.send("Traduisez " + word.english + " en français !");
 
         fs.writeFile(
             [".", "stats", message.author.id + ".txt"].join("/"), 
-            word.english, 
+            word.french, 
             (err) => {
                 if (err) console.log(err.message);
             }
--- code/main.js
+++ code/main.js
@@ -8,8 +8,8 @@ const client = new Discord.Client();
 console.log(functions.get_formatted_date() + "Starting...");
 
 const commandFiles = fs
-    .readdirSync('./commands')
-    .filter((file) => file.endsWith('.js'));
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js"));
 
 client.commands = new Discord.Collection();
 for (const file of commandFiles) {
@@ -17,11 +17,11 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
+client.once("ready", () => {
 	console.log(functions.get_formatted_date() + "BOT \"Translation please\" is now ready!\n");
 });
 
-client.on('message', (message) => {
+client.on("message", (message) => {
 	fs.mkdirSync([".", "debug", message.guild.name].join("/"), { recursive: true });
 	fs.appendFile(
 		[".", "debug", message.guild.name, message.channel.name + ".txt"].join("/"), 
@@ -29,6 +29,17 @@ client.on('message', (message) => {
 		(err) => {
 			if (err) console.log(err.message);
 	});
+
+	if (fs.existsSync("./stats/" + message.author.id + ".txt")) {
+		let valid_word = fs.readFileSync("./stats/" + message.author.id + ".txt", 'utf8').toLowerCase();
+		if (message.content.toLowerCase() == valid_word) {
+			message.reply("Congratulations! You found the right word!");
+		}
+		else {
+			message.reply("You failed... the word was " + valid_word + ".");
+		}
+		fs.unlinkSync("./stats/" + message.author.id + ".txt");
+	}
 
 	if (!message.content.startsWith(config.prefix)) return;
 	if (message.author.bot) return;
