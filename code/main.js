f79a5f482325dbbd823686173312ed03abc0f795
Antoine Félix <felixantoine67@yahoo.fr>
2020-06-07 21:37:25 +0200

    ADDED proper command architecture.

--- code/main.js
+++ code/main.js
@@ -1,15 +1,27 @@
 const auth = require('./auth.json');
 const functions = require('./functions.js');
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-console.log(functions.get_formatted_date() + "[Starting...]\n");
-client.login(auth.token);
+console.log(functions.get_formatted_date() + "Starting...");
+
+const commandFiles = fs
+    .readdirSync('./commands')
+    .filter((file) => file.endsWith('.js'));
+
+client.commands = new Discord.Collection();
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
-	console.log(functions.get_formatted_date() + "[BOT Translation please is now ready]\n\n");
+	console.log(functions.get_formatted_date() + "BOT \"Translation please\" is now ready!\n");
 });
 
 client.on('message', () => {
 	console.log(message.content);
 });
+
+client.login(auth.token);
