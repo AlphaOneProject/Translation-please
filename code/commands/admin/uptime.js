b44eec7169729d011e657bd0175f342ba931c2d1
Felix Antoine <antoine.felix@insa-cvl.fr>
2021-02-14 05:22:18 +0100

    Rewrote uptime in a more flexible way.

--- code/commands/admin/uptime.js
+++ code/commands/admin/uptime.js
@@ -1,6 +1,14 @@
 var config = require("../../config.json");
 const fs = require("fs");
 
+const time_units = {
+    "d": (24 * 60 * 60 * 1000),
+    "h": (60 * 60 * 1000),
+    "m": (60 * 1000),
+    "s": 1000,
+    "ms": 1
+}
+
 module.exports = {
     name: "uptime",
     description: "Display the time elapsed since the bot's boot (reset upon reboot).",
@@ -9,11 +17,11 @@ module.exports = {
     execute(client, message) {
         let uptime = client.uptime;
         let str = "Time elapsed since last boot: \`";
-        str += `${Math.floor(uptime / (24 * 60 * 60 * 1000))}d`; uptime -= (24 * 60 * 60 * 1000) * Math.floor(uptime / (24 * 60 * 60 * 1000))
-        str += ` ${Math.floor(uptime / (60 * 60 * 1000))}h`; uptime -= (60 * 60 * 1000) * Math.floor(uptime / (60 * 60 * 1000))
-        str += ` ${Math.floor(uptime / (60 * 1000))}m`; uptime -= (60 * 1000) * Math.floor(uptime / (60 * 1000))
-        str += ` ${Math.floor(uptime / 1000)}s`; uptime -= 1000 * Math.floor(uptime / 1000)
-        str += ` ${uptime}ms\``;
+        for (const unit of Object.keys(time_units)) {
+            str += ` ${Math.floor(uptime / time_units[unit])}${unit}`;
+            uptime -= time_units[unit] * Math.floor(uptime / time_units[unit])
+        }
+        str += `\``;
         message.channel.send(str);
     },
 };
